0108005fbe3d658e005d7a5ff12a5f9c1884c4be
H-mogusu <ogachi98@gmail.com>
2023-05-02 23:02:56 +0200

    updated the site.yaml to give correct response

--- application2.py
+++ application2.py
@@ -13,7 +13,7 @@ app = flask.Flask(__name__)
 #define route fot the root URL and call the indexed function
 #defien the index function which gets time.
 #return a string with current time/hostname and IP address
-@app.route('/apihealth')
+@app.route('/')
 def index():
     Time= time.strftime("%H:%M:%S")
     return Time+" API server Serving from "+fqdn+" ("+IP_addres+")\n <3 !\n"
